b1b9bc55e9cb52e02b35f7ff62a04aadf4dc7679
Ben Shyong <likeachild@gmail.com>
2012-12-22 21:17:38 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rfairy::Application.config.secret_token = 'd633cadcd5148071c79ef1fe831aab52deb7f4bb7fad1b1437253f1bc9e1d424dbf2fc92a8ff0af7375151d0371cc7478b9231918d51a587e31b44913c18f249'
